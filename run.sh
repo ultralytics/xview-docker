82769291938877e4278016766f4976da3de1cdb3
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-27 01:43:26 +0200

    updates

--- run.sh
+++ run.sh
@@ -5,14 +5,14 @@
 # example:
 # bash run.sh ./1047.tif ./tmp
 
-# GCP:
-cp yolo/checkpoints/*.pt x.pt
-python3
-import torch; a=torch.load('x.pt', map_location='cpu')['model']; torch.save(a,'x.pt'); exit()
-
-sudo rm -rf xview && git clone https://github.com/ultralytics/xview && cp x.pt xview/checkpoint.pt
-sudo docker image prune -a && cd xview && chmod +x run.sh && sudo docker build -t friendlyhello . && sudo docker tag friendlyhello ultralytics/xview:v19
-time sudo docker run -it --memory=8g --cpus=1 ultralytics/xview:v19 bash -c './run.sh /1047.tif /tmp && cat /tmp/1047.tif.txt'
+## GCP:
+#cp yolo/checkpoints/*.pt x.pt
+#python3
+#import torch; a=torch.load('x.pt', map_location='cpu')['model']; torch.save(a,'x.pt'); exit()
+#
+#sudo rm -rf xview && git clone https://github.com/ultralytics/xview && cp x.pt xview/checkpoint.pt
+#sudo docker image prune -a && cd xview && chmod +x run.sh && sudo docker build -t friendlyhello . && sudo docker tag friendlyhello ultralytics/xview:v19
+#time sudo docker run -it --memory=8g --cpus=1 ultralytics/xview:v19 bash -c './run.sh /1047.tif /tmp && cat /tmp/1047.tif.txt'
 # sudo docker push ultralytics/xview:v19
 
 python3 detect.py -image_folder $1 -output_folder $2
