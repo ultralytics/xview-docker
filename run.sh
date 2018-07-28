86650edf6382d4b2207a3cae2f23a3fbb8a2e987
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-07-28 14:37:10 +0200

    updates

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@
 #python3
 #import torch; a=torch.load('x.pt', map_location='cpu')['model']; torch.save(a,'x.pt'); exit()
 #
-#sudo rm -rf xview && git clone https://github.com/ultralytics/xview && cp x.pt xview/checkpoint.pt && cp yolo/models.py xview/models.py
-#sudo docker image prune -a && cd xview && chmod +x run.sh && sudo docker build -t friendlyhello . && sudo docker tag friendlyhello ultralytics/xview:v20
-#time sudo docker run -it --memory=8g --cpus=1 ultralytics/xview:v20 bash -c './run.sh /1047.tif /tmp && cat /tmp/1047.tif.txt'
-# sudo docker push ultralytics/xview:v20
+#sudo rm -rf xview && git clone https://github.com/ultralytics/xview && cp fresh9_5_e201.pt xview/checkpoint.pt
+#sudo docker image prune -a && cd xview && chmod +x run.sh && sudo docker build -t friendlyhello . && sudo docker tag friendlyhello ultralytics/xview:v21
+#time sudo docker run -it --memory=8g --cpus=1 ultralytics/xview:v21 bash -c './run.sh /1047.tif /tmp && cat /tmp/1047.tif.txt'
+# sudo docker push ultralytics/xview:v21
 
 python3 detect.py -image_folder $1 -output_folder $2
