9765056ec0f6f99f28cd7137825ecc21839c5231
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-08-03 20:59:13 +0200

    updates to include classifier

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@
 # import torch; a=torch.load('x.pt', map_location='cpu')['model']; torch.save(a,'x.pt'); exit()
 #
 # sudo rm -rf xview && git clone https://github.com/ultralytics/xview
-# gsutil cp gs://ultralytics/fresh9_5_e201.pt xview/checkpoints/latest.pt && gsutil cp gs://ultralytics/6layer_submit.pt xview/checkpoints/classifier.pt
-# sudo docker image prune -a && cd xview && chmod +x run.sh && sudo docker build -t friendlyhello . && sudo docker tag friendlyhello ultralytics/xview:v22
-# time sudo docker run -it --memory=8g --cpus=1 ultralytics/xview:v22 bash -c './run.sh /1047.tif /tmp && cat /tmp/1047.tif.txt'
-# sudo docker push ultralytics/xview:v22
+# gsutil cp gs://ultralytics/fresh9_5_e201.pt xview/checkpoints/latest.pt && gsutil cp gs://ultralytics/6leaky573.pt xview/checkpoints/classifier.pt
+# sudo docker image prune -a && cd xview && chmod +x run.sh && sudo docker build -t friendlyhello . && sudo docker tag friendlyhello ultralytics/xview:v23
+# time sudo docker run -it --memory=8g --cpus=1 ultralytics/xview:v23 bash -c './run.sh /1047.tif /tmp && cat /tmp/1047.tif.txt'
+# sudo docker push ultralytics/xview:v23
 
 python3 detect.py -image_folder $1 -output_folder $2
